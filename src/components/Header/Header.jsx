c4f4ea92a23dedc56fdf8f7be811b40ff4facf7c
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-11-03 17:58:35 +0100

    Project3

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box } from "@mui/material";
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <h1>d</h1>
+        </div> 
+    )
+}
